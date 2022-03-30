2df14891af9f0e3800057670c9b60a660a2fc798
Julija <julyasya2017@gmail.com>
2022-03-30 18:35:10 +0300

    test commit

---
+++ app.js
@@ -0,0 +1,79 @@
+
+//Работа с массивами
+//1
+//let arr = ['a', 'b', 'c']
+//alert(arr[0] + arr[1] + arr[2])
+//console.log(arr[0] + arr[1] + arr[2])
+
+//2
+//let arr = ['a', 'b', 'c']
+//alert(arr[0])
+//console.log(arr[0])
+//alert(arr[1])
+//console.log(arr[1])
+//alert(arr[2])
+//console.log(arr[2])
+
+//3
+//let arr = ['a', 'b', 'c', 'd']
+//alert (['a + b', 'c + d'])
+//console.log(['a + b'], ['c + d'])
+
+//4
+//let arr = [2, 5, 3, 9]
+//alert ([2 * 5 + 3 * 9])
+//result ([2 * 5 + 3 * 9])
+//console.log([2 * 5 + 3 * 9])
+
+//Объекты (ассоциативные массивы)
+//5???
+//let obj = {a: 1, b: 2, c: 3}
+//alert (obj [c])
+//console.log (obj [c])
+
+//let obj = {a: 1, b: 2, c: 3}
+//alert (obj.c)
+//console.log (obj.c)
+
+
+//6
+//let obj = {'Коля': '1000', 'Вася': '500', 'Петя': '200'}
+//alert(obj['Коля'])
+//alert (obj['Петя'])
+//console.log(obj['Коля'])
+//console.log (obj['Петя'])
+
+//7
+//let obj = {1: 'пн', 2: 'вт', 3: 'ср', 4: 'чт', 5: 'пт', 6: 'сб', 7: 'вс'}
+//alert (obj [2])
+//console.log (obj [2])
+
+// 8
+//let day = {3: 'ср'}
+//alert (day [3])
+//console.log (day [3])
+
+//Многомерные массивы
+//9????
+//let arr = [ [1, 2, 3], [4, 5, 6], [7,8,9] ]
+//alert (arr [2][3])
+//console.log (arr [2][3])
+
+//10 Дан объект {js:['jQuery', 'Angular'], php: 'hello', css: 'world'}. Выведите с его помощью слово 'jQuery'
+
+//let arr = {js:['jQuery', 'Angular'], php: 'hello', css: 'world'}
+//alert (arr [js]['0'])
+//console.log (arr [js] ['0'])
+
+//11
+//let day = {
+ //   'ua': ['пн', 'вт','ср','чт','пт','сб','вс' ],
+//    'en': ['mo', 'tu', 'we','th','fr','sa','su'],
+//}
+//alert (day ['ua']['0'])
+//console.log (day ['ua']['0'])
+//alert (day ['en']['2'])
+//console.log (day ['en']['2'])
+
+//12
+
